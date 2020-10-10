2ce2887261be349ff7026a87e7d592ed74ef4e63
Rafael sanchez <eduardoxlau@hotmail.com>
2020-10-09 21:43:58 -0500

    create search bar component

--- components/searchBar/index.js
+++ components/searchBar/index.js
@@ -1,5 +1,21 @@
+import { Container as Content, Input, Img } from "ui";
+import { Container, Navbar, Form } from "react-bootstrap";
+
 const SearchBar = () => {
-  return <div>Header</div>;
+  return (
+    <Content bg="#fee601">
+      <Container>
+        <Content display="flex" alignItems="center">
+          <Navbar>
+            <Navbar.Brand href="#">
+              <Img src="/Logo_ML.png" alt="logo mercadolibre" />
+            </Navbar.Brand>
+          </Navbar>
+          <Input borderRadius={2} placeholder="Nunca dejes de buscar" />
+        </Content>
+      </Container>
+    </Content>
+  );
 };
 
 export default SearchBar;
