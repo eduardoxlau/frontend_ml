2ab1d0b6abacf1cd9b9bb7ec976713860c94fd79
Rafael sanchez <eduardoxlau@hotmail.com>
2020-10-09 21:43:34 -0500

    create breadcrumb component

--- components/breadcrumb/index.js
+++ components/breadcrumb/index.js
@@ -1,5 +1,13 @@
-const Home = () => {
-  return <div>Header</div>;
+import { Breadcrumb } from "react-bootstrap";
+
+const BreadcrumbCustom = () => {
+  return (
+    <Breadcrumb>
+      <Breadcrumb.Item>Home</Breadcrumb.Item>
+      <Breadcrumb.Item>Library</Breadcrumb.Item>
+      <Breadcrumb.Item active>Data</Breadcrumb.Item>
+    </Breadcrumb>
+  );
 };
 
-export default Home;
+export default BreadcrumbCustom;
